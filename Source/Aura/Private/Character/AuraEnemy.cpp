d4737b8bb302278b15dd0fa13cfbe4de27a2c0be
jiezai <1569596283@qq.com>
2026-03-26 19:39:48 +0800

    鼠标指向敌人

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,3 +3,12 @@
 
 #include "Character/AuraEnemy.h"
 
+void AAuraEnemy::HighlighActor()
+{
+	bHighlighted = true;
+}
+
+void AAuraEnemy::UnHighlightActor()
+{
+	bHighlighted = false;
+}
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -5,10 +5,44 @@
 
 #include <EnhancedInputSubsystems.h>
 #include "EnhancedInputComponent.h"
+#include <Interaction/EnemyInterface.h>
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace() {
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit)return;
+
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+	if (LastActor == nullptr) {
+		if (ThisActor != nullptr) {
+			ThisActor->HighlighActor();
+		}
+	}
+	else {
+		if (ThisActor == nullptr) {
+			LastActor->UnHighlightActor();
+		}
+		else {
+			if ( LastActor != ThisActor) {
+				LastActor->UnHighlightActor();
+				ThisActor->HighlighActor();
+			}
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
